610d7fbd2b7720457b461e8ae76ca5afb1a0ed6d
Josep Rodriguez <josep.rodriguez@tigerspike.com>
2019-10-30 11:52:45 +0000

    Fix issue in require

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = {
-    BitbucketAPI: require('src/BitbucketAPI'),
-    PullRequestAnalysis: require('src/PullRequestAnalysis')
+    BitbucketAPI: require('./src/BitbucketAPI'),
+    PullRequestAnalysis: require('./src/PullRequestAnalysis')
 }
